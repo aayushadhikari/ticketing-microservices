9f4ba986bd1b490ea7a1da562980de2ee6f83fc1
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-08-28 14:47:41 -0400

    email

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signup", (req, res) => {
+  const { email, password } = req.body;
   res.send("Hi There!");
 });
 
