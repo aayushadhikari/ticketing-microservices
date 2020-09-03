eb46348a30d979430e5c5139fe0d80cd84c66666
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-09-02 21:13:21 -0400

    userdoc

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -9,8 +9,14 @@ interface UserAttrs {
 
 //interface that descibes the properties that user model has
 
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+//infercase that describe properties that user document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -28,7 +34,7 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };
 User.build({
